7dfd320389e6efb0d5c3a1dc7df7b1df06dd5c65
Eren Altun <eren.alltun@gmail.com>
2026-03-30 14:31:13 +0300

    Logback-spring.xml oluşturuldu

--- src/main/java/org/erenaltun/countriesbe/CountriesBeApplication.java
+++ src/main/java/org/erenaltun/countriesbe/CountriesBeApplication.java
@@ -1,12 +1,15 @@
 package org.erenaltun.countriesbe;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class CountriesBeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CountriesBeApplication.class, args);
     }
 
